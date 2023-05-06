fbb267d5c01badd0c2f9bd940c806789f00c5e0a
leoCardosoDev <leocardosodev@gmail.com>
2023-05-06 07:43:37 -0300

    feat: add enum to 12

---
+++ bin/12_Enum.dart
@@ -0,0 +1,22 @@
+enum AccountType {free, premium, vip}
+
+void main(List<String> arguments) {
+  final userAccountType = AccountType.vip;
+  print(userAccountType.index);
+  final account = AccountType.values[1];
+  print(account);
+  print(arguments);
+
+  switch (userAccountType) {
+    case AccountType.free:
+      print('0 USD');
+      break;
+    case AccountType.premium:
+      print('20 USD');
+      break;
+    case AccountType.vip:
+      print('50 USD');
+      break;
+    default: break;
+  }
+}
