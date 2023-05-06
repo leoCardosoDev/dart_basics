66a80bd32f6b1434145337dfc2f9cc3d4760bbdd
leoCardosoDev <leocardosodev@gmail.com>
2023-05-06 07:09:35 -0300

    feat: add collection transformation

---
+++ bin/10_Collection_Tranformation.dart
@@ -0,0 +1,19 @@
+void main(List<String> arguments) {
+  final names = ['Leonardo', 'Priscila', 'Mattew'];
+
+  final nameLengths = names.map((name) => name.length).toList();
+  final name = nameLengths[0];
+  print(name);
+
+  final namesFiltered = names.where((name) => name.length == 8).toList();
+  for (int i = 0; i < namesFiltered.length; i++) {
+    print(namesFiltered[i]);
+  }
+
+  for(final name in namesFiltered) {
+    print('Other for $name');
+  }
+
+  namesFiltered.forEach((name) => print('ForEach $name'));
+  namesFiltered.forEach(print);
+}
