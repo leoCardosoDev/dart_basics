b8616790e8cee40a2612a355e42f0029bab7d606
leoCardosoDev <leocardosodev@gmail.com>
2023-05-06 08:35:08 -0300

    feat: add static method to class

--- bin/14_Classes_Static_Members.dart
+++ bin/14_Classes_Static_Members.dart
@@ -2,6 +2,8 @@ void main(List<String> arguments) {
   User user1 = const User(name: 'John', photoUrl: 'http://example.com/abc');
   print(user1.name);
   print(user1.hasLongName());
+
+  print(User.myNameLength);
 }
 
 class User {
@@ -13,4 +15,8 @@ class User {
   bool hasLongName() {
     return name.length > 10;
   }
+
+  static void myMethod() {}
+
+  static const int myNameLength = 3;
 }
