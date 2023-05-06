1b515fcc76bbf676b536b5af74efff629bfe7406
leoCardosoDev <leocardosodev@gmail.com>
2023-05-06 07:23:59 -0300

    feat: add collection 11

---
+++ bin/11_Collection_If_For_Spread.dart
@@ -0,0 +1,25 @@
+void main(List<String> arguments) {
+  //bool isSignedIn = true;
+  <String>[
+    'This is a fake content',
+    //if (isSignedIn) 'Sign out' else 'Sign In',
+  ];
+
+  final x = <String>[
+    for (int i = 0; i < 5; i++) i.toString(),
+    for (final number in [1,2,2,4]) number.toString()
+  ];
+
+  print(x);
+  final listOne = ['hello', 'there', 'hi'];
+  final listTwo = ['what', 'up', 'those'];
+
+  final twoList = <String>[
+    ...listOne,
+    ...listTwo
+  ];
+
+  for (final list in twoList) {
+    print(list);
+  }
+}
